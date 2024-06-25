40d6e76ac0d119285b01097495033a14f26fe2e8
Narasimha <kondanarasimha123@gmail.com>
2024-06-25 16:24:02 +0530

    Starting files

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const AppLayout = ()=> {
+
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AppLayout/>);
