74506a0571a73c46d3d010471563af9ea09b5ce6
Narasimha <kondanarasimha123@gmail.com>
2024-06-26 17:42:54 +0530

    Build Header and Card Components

---
+++ data.js
@@ -0,0 +1,99 @@
+export const data = {
+                      "id": "618037",
+                      "name": "Jai Ganesh Bhojnalaya",
+                      "cloudinaryImageId": "yzgqriufpzmloogcn2vl",
+                      "locality": "Railway Colony",
+                      "areaName": "Bus stand",
+                      "costForTwo": "₹200 for two",
+                      "cuisines": [
+                        "North Indian",
+                        "South Indian",
+                        "Indian",
+                        "Chinese"
+                      ],
+                      "avgRating": 3.8,
+                      "veg": true,
+                      "parentId": "368432",
+                      "avgRatingString": "3.8",
+                      "totalRatingsString": "100+",
+                      "sla": {
+                        "deliveryTime": 50,
+                        "lastMileTravel": 11,
+                        "serviceability": "SERVICEABLE",
+                        "slaString": "45-50 mins",
+                        "lastMileTravelString": "11.0 km",
+                        "iconType": "ICON_TYPE_EMPTY"
+                      },
+                      "availability": {
+                        "nextCloseTime": "2024-06-26 23:00:00",
+                        "opened": true
+                      },
+                      "badges": {
+                        "imageBadges": [
+                          {
+                            "imageId": "v1695133679/badges/Pure_Veg111.png",
+                            "description": "pureveg"
+                          }
+                        ]
+                      },
+                      "isOpen": true,
+                      "type": "F",
+                      "badgesV2": {
+                        "entityBadges": {
+                          "imageBased": {
+                            "badgeObject": [
+                              {
+                                "attributes": {
+                                  "description": "pureveg",
+                                  "imageId": "v1695133679/badges/Pure_Veg111.png"
+                                }
+                              }
+                            ]
+                          },
+                          "textBased": {
+
+                          },
+                          "textExtendedBadges": {
+
+                          }
+                        }
+                      },
+                      "aggregatedDiscountInfoV3": {
+                        "header": "₹100 OFF",
+                        "subHeader": "ABOVE ₹179",
+                        "discountTag": "FLAT DEAL"
+                      },
+                      "orderabilityCommunication": {
+                        "title": {
+
+                        },
+                        "subTitle": {
+
+                        },
+                        "message": {
+
+                        },
+                        "customIcon": {
+
+                        }
+                      },
+                      "differentiatedUi": {
+                        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+                        "differentiatedUiMediaDetails": {
+                          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+                          "lottie": {
+
+                          },
+                          "video": {
+
+                          }
+                        }
+                      },
+                      "reviewsSummary": {
+
+                      },
+                      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+                      "restaurantOfferPresentationInfo": {
+
+                      }
+                    }
--- index.js
+++ index.js
@@ -1,10 +1,84 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import logo from './Images/ONDC-logo.png';
+import biryaniImg from './Images/mario-raj-ysmeQt1dzcw-unsplash.jpg';
+import deliveryIcon from './Images/directions_bike_24dp_FILL0_wght400_GRAD0_opsz24.png';
+import ratingIcon from './Images/star_half_24dp_FILL0_wght400_GRAD0_opsz24.png';
+import { data } from './data.js';
 
-const AppLayout = ()=> {
 
-}
+const Header = ()=> (
+  <div className='header-container'>
+    <div className='logo-container'>
+    <img src={logo} alt="logo-png"/>
+    </div>
+    <div className='nav-container'>
+      <ul>
+        <li>HOME</li>
+        <li>CONTACT</li>
+        <li>ABOUT</li>
+        <li>CART</li>
+      </ul>
+    </div>
+  </div>
+);
+
+const Search = ()=> (
+  <div>
+    <input />
+  </div>
+)
+
+const CardDetails = ()=> (
+  <div className='resturent-card-container'>
+  <div className='resturent-img-container'>
+    <img src={biryaniImg}/>
+    <div className='resturent-details-container'>
+      <h2>Bilal</h2>
+
+      <div className='resturent-delivery-container'>
+      <img src={deliveryIcon}/>
+      <p>15-20 Min</p>
+      </div>
+
+      <div className='resturent-rating-container'>
+      <img src={ratingIcon}/>
+      <p>4.5</p>
+      </div>
+
+      <div className='priceCents'>
+        <h3>TWO FOR 200 ₹</h3>
+      </div>
+
+      <h6>Biryani, chicken, Mutton, southindia</h6>
+      <h4>Ongole</h4>
+
+    </div>
+  </div>
+</div>
+)
+
+
+const Card = ()=> (
+  <div className='card-container'>
+    <CardDetails/>
+    <CardDetails/>
+    <CardDetails/>
+    <CardDetails/>
+  </div>
+)
+
+
+const AppLayout = ()=> (
+  <div className='app'>
+    <Header/>
+    <Search/>
+    <Card/>
+  </div>
+)
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<AppLayout/>);
